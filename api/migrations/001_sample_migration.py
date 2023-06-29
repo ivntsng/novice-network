3a822ce53419f36757256e51e6efcd77e0b88e60
Dream Apitchaya <apitchayatramirez@gmail.com>
2023-06-29 15:27:05 -0400

    create posts table

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -35,4 +35,18 @@ steps = [
         DROP TABLE big_dummy;
         """
     ]
+    [
+        """
+        CREATE TABLE posts (
+            id SERIAL PRIMARY KEY NOT NULL,
+            title VARCHAR (1000) NOT NULL,
+            created_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            description TEXT NOT NULL,
+        );
+        """,
+
+        """
+        DROP TABLE posts;
+        """
+    ]
 ]
