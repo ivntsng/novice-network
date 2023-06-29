3d8b49b3f72bdbce75d43cc551fda68a34d60398
Dream Apitchaya <apitchayatramirez@gmail.com>
2023-06-29 17:11:14 -0400

    migration table done

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -1,52 +1,16 @@
 steps = [
     [
-        # "Up" SQL statement
-        """
-        CREATE TABLE dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
-        );
-        """,
-        # "Down" SQL statement
-        """
-        DROP TABLE dummy;
-        """
-    ],
-    [
-        # "Up" SQL statement
-        """
-        CREATE TABLE big_dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
-        );
-        """,
-        # "Down" SQL statement
-        """
-        DROP TABLE big_dummy;
-        """
-    ]
-    [
         """
         CREATE TABLE posts (
             id SERIAL PRIMARY KEY NOT NULL,
             title VARCHAR (1000) NOT NULL,
             created_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            description TEXT NOT NULL,
+            description TEXT NOT NULL
         );
         """,
 
         """
         DROP TABLE posts;
-        """
-    ]
+        """,
+    ],
 ]
