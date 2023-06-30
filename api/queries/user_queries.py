457431bf113a19fbcf22b05ba86aabb5324387c6
Andrew Morales <moralesandrew2@gmail.com>
2023-06-30 15:51:31 -0700

    working update user endpoint

--- api/queries/user_queries.py
+++ api/queries/user_queries.py
@@ -1,8 +1,4 @@
-import os
-from psycopg_pool import ConnectionPool
-
-
-pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
+from queries.pool import pool
 
 
 class UserQueries:
@@ -19,6 +15,38 @@ class UserQueries:
                     """
                     INSERT INTO users (username, password, email, type)
                     VALUES (%s, %s, %s, %s)
+                    RETURNING id, username, password, email, type
+                    """,
+                    params,
+                )
+
+                record = None
+                row = cur.fetchone()
+                if row is not None:
+                    record = {}
+                    for i, column in enumerate(cur.description):
+                        record[column.name] = row[i]
+
+                return record
+
+    def update_user(self, user_id, data):
+        with pool.connection() as conn:
+            with conn.cursor() as cur:
+                params = [
+                    data.username,
+                    data.password,
+                    data.email,
+                    data.type,
+                    user_id,
+                ]
+                cur.execute(
+                    """
+                    UPDATE users
+                    SET username = %s
+                      , password = %s
+                      , email = %s
+                      , type = %s
+                    WHERE id = %s
                     RETURNING id, username, password, email, type
                     """,
                     params,
