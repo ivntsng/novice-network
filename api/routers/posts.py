a4833d89b4edfa9a4a8ecdec2ddaa5fd28debc84
Apitchaya Ramirez <apitchayatramirez@gmail.com>
2023-07-10 21:47:51 +0000

    Finish posts endpoint

--- api/routers/posts.py
+++ api/routers/posts.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, Response
-from typing import Union, List
+from typing import Union, List, Optional
 from queries.posts import (
     PostIn,
     PostRepository,
@@ -22,3 +22,29 @@ def get_all(
     repo: PostRepository = Depends(),
 ):
     return repo.get_all()
+
+@router.put("/posts/{post_id}", response_model=Union[PostOut, Error])
+def update_post(
+    post_id: int,
+    post: PostIn,
+    repo: PostRepository = Depends(),
+) -> Union[PostOut, Error]:
+    return repo.update(post_id, post)
+
+@router.get("/posts/{post_id}", response_model=Optional[PostOut])
+def get_one_post(
+    post_id: int,
+    response: Response,
+    repo: PostRepository = Depends(),
+) -> PostOut:
+    post = repo.get_one(post_id)
+    if post is None:
+        response.status_code = 404
+    return post
+
+@router.delete("/posts/{post_id}", response_model=bool)
+def delete_post(
+    post_id: int,
+    repo: PostRepository = Depends(),
+) -> bool:
+    return repo.delete(post_id)
