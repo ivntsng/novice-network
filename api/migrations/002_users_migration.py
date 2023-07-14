48dea69312bd327496cdb7f7d3677442f0df18e2
Apitchaya Ramirez <apitchayatramirez@gmail.com>
2023-07-14 21:43:45 +0000

    Backend Authorization

--- api/migrations/002_users_migration.py
+++ api/migrations/002_users_migration.py
@@ -2,18 +2,17 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE users (
-            user_id SERIAL PRIMARY KEY NOT NULL,
+        CREATE TABLE accounts (
+            id SERIAL PRIMARY KEY NOT NULL,
             username VARCHAR(1000) NOT NULL UNIQUE,
             hashed_password varchar(200) NOT NULL,
             email VARCHAR(1000) NOT NULL UNIQUE,
-            created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             role VARCHAR(1000) NOT NULL
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE users;
+        DROP TABLE accounts;
         """,
     ],
 ]
