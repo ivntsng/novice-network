ca184d775c293ec71363c0b47aa80863482587f3
Apitchaya Ramirez <apitchayatramirez@gmail.com>
2023-07-17 21:42:30 +0000

    Delete post

--- ghi/src/PostDetail.js
+++ ghi/src/PostDetail.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 
-function PostDetail() {
+function PostDetail({ getPosts }) {
     const { post_id } = useParams();
-
+    const navigate = useNavigate();
     const [post, setPost] = useState(null);
+    const onDelete = async () => {
+        const confirmed = window.confirm('Are you sure you want to delete this post?');
+        if (confirmed) {
+            try {
+            const response = await fetch(`http://localhost:8000/posts/${post_id}`, {
+                method: 'DELETE',
+            });
 
+            if (response.ok) {
+                navigate('/posts'); // Redirect to the posts page
+                getPosts();
+            } else {
+                console.log('Error deleting post');
+            }
+            } catch (error) {
+            console.log('Error deleting post:', error);
+            }
+        }
+        };
     useEffect(() => {
         const fetchPostDetails = async () => {
         try {
@@ -45,28 +63,40 @@ function PostDetail() {
     return (
         <>
         <section>
-        <div className="container my-5 py-5 text-dark">
-            <div className="row d-flex justify-content-center">
-            <div className="col-md-11 col-lg-9 col-xl-7">
-                <div className="d-flex flex-start mb-4">
-                <img className="rounded-circle shadow-1-strong me-3"
-                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar" width="65"
-                    height="65" />
-                <div className="card w-100">
-                    <div className="card-body p-4">
-                        <div className="">
-                            <h4>{post.title}</h4>
-                            <p className="medium">@username {post.owner_id}</p>
-                            <p className="small">Post created: {formattedDate} {formattedTime}</p>
-                            <p>{post.description}</p>
+            <div className="container my-5 py-5 text-dark">
+                <div className="row d-flex justify-content-center">
+                    <div className="col-md-11 col-lg-9 col-xl-7">
+                        <div className="d-flex flex-start mb-4">
+                         <img className="rounded-circle shadow-1-strong me-3"
+                            src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar" width="65"
+                            height="65" />
+                            <div className="card w-100">
+                                <div className="card-body p-4">
+                                    <div className="">
+                                        <h4>{post.title}</h4>
+                                        <p className="medium">@username {post.owner_id}</p>
+                                        <p className="small">Post created: {formattedDate} {formattedTime}</p>
+                                        <p>{post.description}</p>
+                                        <div className="align-items-right">
+                                            <Link
+                                                href="#!" style={{ color: 'gray' }}>
+                                                    <i className="bi bi-pencil-fill"></i>
+                                                    Edit
+                                            </Link>
+                                            <Link
+                                                onClick={onDelete} style={{ color: 'gray' }}>
+                                                    <i className="bi bi-trash3-fill"></i>
+                                                    Delete
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
 
+                            </div>
                         </div>
                     </div>
                 </div>
-                </div>
             </div>
-            </div>
-        </div>
         </section>
         </>
     );
