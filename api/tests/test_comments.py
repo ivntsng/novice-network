4cc4ee3c5e0ef097f5f59548d463b0629b48e0b4
casper <ivntsng@gmail.com>
2023-07-26 11:48:12 -0700

    Made changes to requirements.txt folder

--- api/tests/test_comments.py
+++ api/tests/test_comments.py
@@ -10,9 +10,11 @@ sys.path.append(project_root)
 
 client = TestClient(app)
 
+
 class EmptyComments:
     def get_all(self, post_id: int):
         return []
+
 
 class CreateComment:
     def create(self, post_id: int, comment: CommentIn) -> CommentOut:
@@ -26,12 +28,14 @@ class CreateComment:
         result.update(comment.dict())
         return CommentOut(**result)
 
+
 def test_get_all_comments():
     app.dependency_overrides[CommentRepository] = EmptyComments
     response = client.get("/posts/1/comments")
     app.dependency_overrides = {}
     assert response.status_code == 200
     assert response.json() == []
+
 
 def test_create_comment():
     app.dependency_overrides[CommentRepository] = CreateComment
