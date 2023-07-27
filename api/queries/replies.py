3030c5308088103aefbda6ca75b3106c4d74c415
Paarth <paarthjoshi00@gmail.com>
2023-07-27 17:42:22 +0000

    added css styling and refactored front end

--- api/queries/replies.py
+++ api/queries/replies.py
@@ -24,7 +24,7 @@ class Error(BaseModel):
 
 
 class ReplyRepository:
-    def get_one(self, reply_id: int) -> Optional[ReplyOut]:
+    def get_one(self, post_id: int, comment_id: int, reply_id: int) -> Optional[ReplyOut]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
@@ -36,9 +36,11 @@ class ReplyRepository:
                                 , reply
                                 , created_on
                         FROM replies
-                        WHERE reply_id = %s
+                        WHERE post_id = %s AND comment_id = %s AND reply_id = %s
                         """,
                         [
+                            post_id,
+                            comment_id,
                             reply_id,
                         ],
                     )
@@ -50,23 +52,23 @@ class ReplyRepository:
             print(e)
             return {"message": "Could not get that reply"}
 
-    def delete(self, reply_id: int) -> bool:
+    def delete(self, post_id: int, comment_id: int, reply_id: int) -> bool:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
                     db.execute(
                         """
                         DELETE FROM replies
-                        WHERE reply_id = %s
+                        WHERE post_id = %s AND comment_id = %s AND reply_id = %s
                         """,
-                        [reply_id],
+                        [post_id, comment_id, reply_id]
                     )
                     return True
         except Exception as e:
             print(e)
             return False
 
-    def update(self, reply_id: int, reply: ReplyIn) -> Union[ReplyOut, Error]:
+    def update(self, post_id: int, comment_id: int, reply_id: int, reply: ReplyIn) -> Union[ReplyOut, Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
@@ -76,12 +78,14 @@ class ReplyRepository:
                         SET owner_username = %s
                            , reply = %s
                            , created_on = %s
-                        WHERE reply_id = %s
+                        WHERE post_id = %s AND comment_id = %s AND reply_id = %s
                         """,
                         [
                             reply.owner_username,
                             reply.reply,
                             reply.created_on,
+                            post_id,
+                            comment_id,
                             reply_id,
                         ],
                     )
@@ -90,7 +94,7 @@ class ReplyRepository:
             print(e)
             return {"message": "Could not update that reply"}
 
-    def get_all(self, comment_id: int) -> Union[Error, List[ReplyOut]]:
+    def get_all(self, post_id: int, comment_id: int) -> Union[Error, List[ReplyOut]]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
@@ -98,10 +102,10 @@ class ReplyRepository:
                         """
                         SELECT reply_id, owner_username, comment_id, reply, created_on
                         FROM replies
-                        WHERE comment_id = %s
+                        WHERE post_id = %s AND comment_id = %s
                         ORDER BY created_on;
                         """,
-                        [comment_id],
+                        [post_id, comment_id]
                     )
                     result = db.fetchall()
                     return [
@@ -118,21 +122,22 @@ class ReplyRepository:
             print(e)
             return {"message": "Could not get all replies"}
 
-    def create(self, reply: ReplyIn) -> Union[ReplyOut, Error]:
+    def create(self, post_id: int, comment_id: int, reply: ReplyIn) -> Union[ReplyOut, Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
                     result = db.execute(
                         """
                         INSERT INTO replies
-                        (owner_username, comment_id, reply, created_on)
+                        (post_id, owner_username, comment_id, reply, created_on)
                         VALUES
-                            (%s, %s, %s, CURRENT_TIMESTAMP)
+                            (%s, %s, %s, %s, CURRENT_TIMESTAMP)
                         RETURNING reply_id;
                         """,
                         [
+                            post_id,
                             reply.owner_username,
-                            reply.comment_id,
+                            comment_id,
                             reply.reply,
                         ],
                     )
@@ -142,9 +147,14 @@ class ReplyRepository:
             print(e)
             return {"message": "Create did not work"}
 
-    def reply_in_to_out(self, id: int, reply: ReplyIn):
-        old_data = reply.dict()
-        return ReplyOut(reply_id=id, **old_data)
+    def reply_in_to_out(self, reply_id: int, reply: ReplyIn) -> ReplyOut:
+        return ReplyOut(
+            reply_id=reply_id,
+            owner_username=reply.owner_username,
+            comment_id=reply.comment_id,
+            reply=reply.reply,
+            created_on=reply.created_on,
+        )
 
     def record_to_reply_out(self, record):
         return ReplyOut(
